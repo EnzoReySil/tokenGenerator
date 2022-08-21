bfe52b3e19e99f1b0d330ef82e7e6d28b8254e14
Juan Lorenzo Reyes Silva <lorenzoreyes62@aragon.unam.mx>
2022-08-21 16:35:02 -0500

    Funcionalidad completada

--- src/main/java/com/cinepolis/tokengenerator/SecurityConfig.java
+++ src/main/java/com/cinepolis/tokengenerator/SecurityConfig.java
@@ -3,18 +3,13 @@ package com.cinepolis.tokengenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfiguration {
+public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic().and().build();
-    }
-
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().antMatchers("/");
+        http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
+        return http.build();
     }
 }
