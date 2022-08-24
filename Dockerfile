49ee0b4f26833f243e54ed889a16425ce777d03f
Juan Lorenzo Reyes Silva <lorenzoreyes62@aragon.unam.mx>
2022-08-24 12:41:55 -0500

    Actualizar Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:8-jdk-alpine
+FROM adoptopenjdk/openjdk11:latest
 VOLUME /tmp
 COPY target/*.jar app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/cinepolis/tokengenerator/controller/TokenGeneratorController.java
+++ src/main/java/com/cinepolis/tokengenerator/controller/TokenGeneratorController.java
@@ -1,20 +1,14 @@
 package com.cinepolis.tokengenerator.controller;
 
 import com.cinepolis.tokengenerator.models.TokenRequestBodyModel;
-import com.fasterxml.jackson.databind.util.JSONPObject;
-import netscape.javascript.JSObject;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 @RestController
 public class TokenGeneratorController {
-
-    // TODO: Implementar JSON con mejores prácticas? (https://www.baeldung.com/jackson-object-mapper-tutorial)
     @PostMapping(value = "/", consumes = "application/json", produces = "application/json")
     public HashMap<String, Object> generateToken(@RequestBody TokenRequestBodyModel tokenRequestBodyModel) {
         HashMap<String, Object> response = new HashMap<>();
